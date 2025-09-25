c32d0f329df7fff29008d09ed27f3bf4061dff2f
Roman Synytskyi <romansinitsky@gmail.com>
2025-09-25 16:39:56 -0400

    outputs fixed

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
 output "instance_ip_addr_1" {
-  value       = aws_instance.my_server[0].public_ip
+  value       = aws_instance.my_server[*].public_ip
   description = "The private IP address of the main server instance."
-}
-
-output "instance_ip_addr_2" {
-  value       = aws_instance.my_server[1].public_ip
-  description = "The private IP address of the backup server instance."
 }
